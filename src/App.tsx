eb4d938c9d6f794ccd4b459bc6447762b9e79839
thomaslaeremans <thomaslaeremans@hotmail.com>
2026-03-17 15:53:13 +0100

    fix basename router

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/portfolio">
         <StarField />
         <Routes>
           <Route path="/" element={<Index />} />
